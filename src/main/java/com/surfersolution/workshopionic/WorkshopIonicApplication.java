94aaeb2103fe421cb0cb383dbf3e3207e11338ac
Kristian Mello <kristian.bonfim@hotmail.com>
2021-11-16 20:44:57 -0300

    Project created

---
+++ src/main/java/com/surfersolution/workshopionic/WorkshopIonicApplication.java
@@ -0,0 +1,13 @@
+package com.surfersolution.workshopionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopIonicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surfersolution/workshopionic/WorkshopIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surfersolution.workshopionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
