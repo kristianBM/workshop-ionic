b17c3b6ee2da1cbcfb1bc07ac5115394dced6ae7
Kristian Mello <kristian.bonfim@hotmail.com>
2021-11-17 10:16:39 -0300

    Instantiation operation for test

--- src/main/java/com/surfersolution/workshopionic/WorkshopIonicApplication.java
+++ src/main/java/com/surfersolution/workshopionic/WorkshopIonicApplication.java
@@ -1,13 +1,34 @@
 package com.surfersolution.workshopionic;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class WorkshopIonicApplication {
+import com.surfersolution.workshopionic.domain.Category;
+import com.surfersolution.workshopionic.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class WorkshopIonicApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopIonicApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+	
+		Category cat1 = new Category(null, "Tech");
+		Category cat2 = new Category(null, "Desk");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
