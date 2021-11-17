2c8c4c5badf92a76f32efe0f05e218250f740024
Kristian Mello <kristian.bonfim@hotmail.com>
2021-11-16 22:06:26 -0300

    test database h2 implementd

--- src/main/java/com/surfersolution/workshopionic/resources/CategoryResource.java
+++ src/main/java/com/surfersolution/workshopionic/resources/CategoryResource.java
@@ -1,5 +1,11 @@
 package com.surfersolution.workshopionic.resources;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categories")
 public class CategoryResource {
 
+	
 }
