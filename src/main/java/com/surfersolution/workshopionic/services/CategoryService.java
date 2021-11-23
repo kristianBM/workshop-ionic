594fb63b567c3d9112187baf9ac1ee11a1cf8358
Kristian Mello <kristian.bonfim@hotmail.com>
2021-11-23 19:12:32 -0300

    Delete Category method

--- src/main/java/com/surfersolution/workshopionic/services/CategoryService.java
+++ src/main/java/com/surfersolution/workshopionic/services/CategoryService.java
@@ -3,10 +3,12 @@ package com.surfersolution.workshopionic.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.surfersolution.workshopionic.domain.Category;
 import com.surfersolution.workshopionic.repositories.CategoryRepository;
+import com.surfersolution.workshopionic.services.exceptions.DataIntegrityException;
 import com.surfersolution.workshopionic.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -31,5 +33,15 @@ public class CategoryService {
 		return categoryRepository.save(obj);
 	}
 	
+	public void delete (Integer id) {
+		findById(id);
+		try {
+			categoryRepository.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e){
+			throw new DataIntegrityException("Can't delete a category which contains products.");
+			
+		}
+	}
 }
 
